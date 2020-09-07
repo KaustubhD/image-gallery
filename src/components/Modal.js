abe22d8dd9f76a3a80ceb718d85783d58187efbe
kaustubh-ex2 <kdhasmana@ex2india.com>
2020-09-07 09:27:15 +0530

    Adds and uses framer motion Package for animation

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const Modal = ({ selectedImage, setSelectedImage }) => {
 	const removeModalHandler = (ev) => {
 		if (ev.target.classList.contains('backdrop')) setSelectedImage(null)
 	}
 	return (
-		<div className="backdrop" onClick={removeModalHandler}>
-			<img src={selectedImage} alt="" />
-		</div>
+		<motion.div
+			className="backdrop"
+			onClick={removeModalHandler}
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+		>
+			<motion.img
+				src={selectedImage}
+				alt=""
+				initial={{ y: '-100vh' }}
+				animate={{ y: 0 }}
+			/>
+		</motion.div>
 	)
 }
 
--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import useStorage from '../hooks/useStorage'
+import { motion } from 'framer-motion'
 
 const ProgressBar = ({ file, setFile }) => {
 	const { url, progress } = useStorage(file)
@@ -7,7 +8,11 @@ const ProgressBar = ({ file, setFile }) => {
 		if (url) setFile(null)
 	}, [url])
 	return (
-		<div className="progress-bar" style={{ width: progress + '%' }}></div>
+		<motion.div
+			className="progress-bar"
+			initial={{ width: 0 }}
+			animate={{ width: progress + '%' }}
+		></motion.div>
 	)
 }
 
